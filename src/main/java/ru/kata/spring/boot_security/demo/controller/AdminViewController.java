f23cd27644bab7ff33f75c7542ef30be1ced6dc8
Mars <sharifullin44@yandex.ru>
2025-05-20 23:04:44 +0500

    реализовал REST controllers, добавил mapper, DTO

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminViewController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminViewController.java
@@ -1,7 +1,10 @@
 package ru.kata.spring.boot_security.demo.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.demo.model.User;
 
 @Controller
 public class AdminViewController {
@@ -9,5 +12,18 @@ public class AdminViewController {
     @GetMapping("/admin")
     public String adminPage() {
         return "admin";
+    }
+
+
+    @GetMapping("/user")
+    public String userPage(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("user", user);
+        return "user";
+    }
+
+    @GetMapping("/")
+    public String redirectToAdmin() {
+        return "redirect:/admin";
+
     }
 }
